25e925b5f667d319ba98828611828ac03e62010d
AS3-1-04 <AS3-1-04@uni.lan>
2019-03-26 13:08:44 +0100

    asaaaa

--- 3_evaluacion/nodejs/ejemplo01/webapp.js
+++ 3_evaluacion/nodejs/ejemplo01/webapp.js
@@ -1,46 +1,7 @@
-/*
-ejemplo 03: for + arrays + JSON
-*/
-
-// https://www.w3schools.com/js/js_loop_for.asp
-// https://www.w3schools.com/js/js_objects.asp
-// https://www.w3schools.com/js/js_arrays.asp
 
 /*
-Definir los datos meteorológicos de la tendencia de los próximos 6 días. Con arrays [] y diccionarios {}
-http://www.euskalmet.euskadi.eus
+https://expressjs.com
 */
 
-tiempo = [
-    {dia:"Miércoles", cielo:"lluvia", temperatura:12},
-    {dia:"Jueves", cielo:"Nublado", temperatura:20},
-    {dia:"Viernes", cielo:"lluvia", temperatura:23},
-    {dia:"Sábado", cielo:"sol", temperatura:10},
-    {dia:"Domingo", cielo:"sol", temperatura:5},
-    {dia:"lunes", cielo:"Nublado", temperatura:0},
-]
-
-/*
-Imprimir los datos usando 'for' y console.log
-*/
-
-/*
-console.log(tiempo[0].dia + " " + tiempo[0].cielo)
-console.log(tiempo[1].dia + " " + tiempo[1].cielo)
-console.log(tiempo[2].dia + " " + tiempo[2].cielo)
-console.log(tiempo[3].dia + " " + tiempo[3].cielo)
-console.log(tiempo[4].dia + " " + tiempo[4].cielo)
-console.log(tiempo[5].dia + " " + tiempo[5].cielo)
-*/
-
-var i;
-for (i = 0; i < tiempo.length; i++){ 
-    if(tiempo[i].cielo == "lluvia"){
-    console.log(tiempo[i].dia + " " + tiempo[i].cielo + " " + tiempo[i].temperatura)
-}}
-
-/*
-Crear una página HTML con los datos anteriores usando las imágenes (sol, nubes, ...)
-nube: http://www.euskalmet.euskadi.eus/appcont/meteorologia/meteodat/images/12.png
-...
-*/
+// crea una web app "Hello World!!"
+// http://[IP]:[port] -> "Hello World!"
