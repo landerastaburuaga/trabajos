221eeb154f9d923524d1363a362bd36492463e0e
AS3-1-04 <AS3-1-04@uni.lan>
2019-04-03 11:18:18 +0200

    aaa

--- 3_evaluacion/nodejs/webapp/app2.js
+++ 3_evaluacion/nodejs/webapp/app2.js
@@ -19,30 +19,27 @@ tiempo = [
     {dia:"Jueves", cielo:"Nublado", temperatura:20},
     {dia:"Viernes", cielo:"lluvia", temperatura:23},
     {dia:"Sábado", cielo:"sol", temperatura:10},
-    {dia:"Domingo", cielo:"sol", temperatura:5},
+    {dia:"Domingo", cielo:"lluvia", temperatura:5},
     {dia:"lunes", cielo:"Nublado", temperatura:0},
 ]
 
 app.get('/tiempo', function (req, res) {
 
-    tiempo = [
-        {dia:"Miércoles", cielo:"lluvia", temperatura:12},
-        {dia:"Jueves", cielo:"Nublado", temperatura:20},
-        {dia:"Viernes", cielo:"lluvia", temperatura:23},
-        {dia:"Sábado", cielo:"sol", temperatura:10},
-        {dia:"Domingo", cielo:"sol", temperatura:5},
-        {dia:"lunes", cielo:"Nublado", temperatura:0},
-    ]
-
-        var i;
-        var r;
-    console.log("Dias llueviosos:")
-    for (i = 0; i < tiempo.length; i++){ 
-        if(tiempo[i].cielo == "lluvia"){
+var r = []
+var i;
+console.log("Dias llueviosos:")
+for (i = 0; i < tiempo.length; i++){ 
+    if(tiempo[i].cielo == "lluvia"){
+    //console.log(tiempo[i].dia + " " + tiempo[i].cielo + " " + tiempo[i].temperatura)
+     //console.log(tiempo[i].dia)
+     //r = tiempo[i].dia
+   r[r.length]=tiempo[i].dia
+    }}
+    
         //console.log(tiempo[i].dia + " " + tiempo[i].cielo + " " + tiempo[i].temperatura)
         //console.log(tiempo[i].dia)
-        //r = tiempo[i].dia
-       r = tiempo[i].dia + " " + tiempo[i].cielo + " " + tiempo[i].temperatura
-    }}
+    
+     
+
     res.send(r)
 });   
